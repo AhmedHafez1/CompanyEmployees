cfbf7f56e051fcc5031f610ebef1ebb90f9169e7
ahafez <ahafez@tafeel.com>
2022-01-20 15:23:45 +0200

    DbContext & Initial Data

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,17 +1,35 @@
 using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
 
 var builder = WebApplication.CreateBuilder(args);
 
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
 // Add services to the container.
 
-builder.Services.AddControllers();
 builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.AddControllers();
+builder.Services.ConfigureLoggerService();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+if (app.Environment.IsDevelopment()) app.UseDeveloperExceptionPage();
+else app.UseHsts();
+
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
---
+++ Repository/RepositoryContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Entities.Models;
+using Repository.Configuration;
+
+namespace Repository
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CompanyConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+        }
+        public DbSet<Company>? Companies { get; set; }
+        public DbSet<Employee>? Employees { get; set; }
+
+    }
+}
